f36242165afb422f04067e22fc838dfe52501a0d
ValeriaRogulina <71660202+ValeriaRogulina@users.noreply.github.com>
2020-10-02 23:46:07 +0300

    Add files via upload

---
+++ OAIP2-3(1).cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float a, b, c;
+
+	printf("Enter a: ");
+	scanf_s("%f", &a);
+
+	printf("Enter b: ");
+	scanf_s("%f", &b);
+
+	printf("Enter c: ");
+	scanf_s("%f", &c);
+
+	float S, p;
+
+	p = (a + b + c) / 2;
+	S = sqrt(p*(p - a)*(p - b)*(p - c));
+
+	printf("S is %.3f\n", S);
+
+	return 0;
+}
---
+++ OAIP2-3(2).cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float a, b, x;
+
+	printf("Enter a: ");
+	scanf_s("%f", &a);
+
+	printf("Enter b: ");
+	scanf_s("%f", &b);
+
+	printf("Enter x: ");
+	scanf_s("%f", &x);
+
+	float y, z;
+
+	y = a * pow(x, 2) - 4 * b;
+
+	if (y != 0) {
+		z = (a * x - b) / y;
+		printf("Z is %.3f\n", z);
+	}
+	else {
+		printf("z not defined");
+	}
+
+	return 0;
+}
